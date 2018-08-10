40b0a274172bcdd4b89090cff67e282a37aa0381
guenni3000 <buch.schal1@hotmail.de>
2018-08-10 21:12:03 +0200

    finished cnn debugging

--- cnn.py
+++ cnn.py
@@ -1,4 +1,6 @@
 import numpy as np
+import skimage
+from skimage import io, transform
 import tensorflow as tf
 from keras.models import Model
 from keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten, Input, Lambda
@@ -7,63 +9,89 @@ from PIL import Image
 import os
 import keras
 
-dir = "C:/Users/Julian/Desktop/dataset/training"
-in_shape = (500, 375, 1)
-comp_filter_size = 5
+dir = "C:/Users/Julian/Desktop/set/output"
+in_shape = (187, 250, 1)
+comp_filter_size = 8
 compare_filters = 5
+batch_size = 8
 file_count = len(os.listdir(os.fsencode(dir)))
 
 def compare(x):
-    dims = tf.shape(x)
+    dims = x.get_shape()
 
-    imgs1 = x[:][:][:][0:dims[3]/2]
-    imgs2 = x[:][:][:][dims[3]/2:]
+    imgs1 = x[:, :, :, 0:int(int(dims[3])/2)]
+    imgs2 = x[:, :, :, int(int(dims[3])/2):]
 
     imgs_out = []
 
-    for b in range(dims[0]):
+    for b in range(batch_size):
         img1 = imgs1[b]
         img2 = tf.expand_dims(imgs2[b], 0)
 
         filters_k = []
 
-        for f in range(comp_filter_size):
+        for f in range(compare_filters):
             
-            for i in range(dims[3]/2):
-                x_coord = np.random.randint(0, dims[1]-comp_filter_size)
-                y_coord = np.random.randint(0, dims[2]-comp_filter_size)
-                filter_new = img1[x_coord:x_coord+comp_filter_size][y_coord:y_coord+comp_filter_size][i]
+            filters = []
 
-                if i == 0:
-                    filters = filter_new
-                else:
-                    filters = np.concatenate((filters, filter_new), 2)
+            for i in range(int(int(dims[3])/2)):
+                x_coord = np.random.randint(0, int(dims[1])-comp_filter_size)
+                y_coord = np.random.randint(0, int(dims[2])-comp_filter_size)
+                filter_new = img1[x_coord:x_coord+comp_filter_size, y_coord:y_coord+comp_filter_size, i]
+
+                filters.append(filter_new)
             
-            filters_k.append(filters)
+            filters_k.append(tf.stack(filters, axis=2))
 
         img_new = tf.nn.conv2d(img2, tf.stack(filters_k, 3), [1, 1, 1, 1], 'SAME')
         imgs_out.append(img_new)
 
-    return tf.parallel_stack(imgs_out)
+    return tf.concat(imgs_out, 0)
 
 def get_imgs(path):
-    for file in os.listdir(os.fsencode(path)):
-        filename = os.fsdecode(file)
-        parts = filename.split('_')
-        
-        if parts[1] == '0':
-            out = parts[2][0]
-            path1 = path+'/'+filename
-            path2 = path+'/'+parts[0]+'_1_'+parts[2]
-            path3 = path+'/'+parts[0]+'_2_'+parts[2]
+    while True:
+        in1 = []
+        in2 = []
+        in_base = []
+        out = []
 
-            yield ({'input1': tf.image.decode_image(tf.read_file(path1), 1), 
-                    'input2': tf.image.decode_image(tf.read_file(path2), 1), 
-                    'input_base': tf.image.decode_image(tf.read_file(path3), 1)}, {'output': out})
+        for file in os.listdir(os.fsencode(path)):
+            filename = os.fsdecode(file)
+            parts = filename.split('_')
 
-input1 = Input(shape=in_shape, dtype='uint8', name='input1')
-input2 = Input(shape=in_shape, dtype='uint8', name='input2')
-input_base = Input(shape=in_shape, dtype='uint8', name='input_base')
+            if parts[1] == '0':
+                output = int(parts[2][0])
+                path1 = path+'/'+filename
+                path2 = path+'/'+parts[0]+'_1_'+parts[2]
+                path3 = path+'/'+parts[0]+'_2_'+parts[2]
+
+                img1 = io.imread(path1, as_gray=True)
+                #img1 = skimage.img_as_ubyte(img1)
+                img1 = np.expand_dims(img1, -1)
+
+                img2 = io.imread(path2, as_gray=True)
+                #img2 = skimage.img_as_ubyte(img2)
+                img2 = np.expand_dims(img2, -1)
+
+                img3 = io.imread(path3, as_gray=True)
+                #img3 = skimage.img_as_ubyte(img3)
+                img3 = np.expand_dims(img3, -1)
+
+                in1.append(img1)
+                in2.append(img2)
+                in_base.append(img3)
+                out.append(output)
+
+            if len(in1) == batch_size:
+                yield ([np.stack(in1), np.stack(in2), np.stack(in_base)], np.stack(out))
+                in1 = []
+                in2 = []
+                in_base = []
+                out = []
+
+input1 = Input(shape=in_shape, name='input1')
+input2 = Input(shape=in_shape, name='input2')
+input_base = Input(shape=in_shape, name='input_base')
 
 conv_layer = Conv2D(16, (3, 3), padding='same', activation='relu')
 
@@ -91,6 +119,9 @@ comp2 = compare_layer(merged2)
 comp1 = pooling_layer(comp1)
 comp2 = pooling_layer(comp2)
 
+comp1 = Dropout(0.25)(comp1)
+comp2 = Dropout(0.25)(comp2)
+
 comp1 = Flatten()(comp1)
 comp2 = Flatten()(comp2)
 
@@ -102,6 +133,6 @@ x = Dense(64, activation='relu')(x)
 
 output = Dense(1, activation='sigmoid', name='output')(x)
 
-model = Model(inputs=[input1, input2], outputs=[output])
+model = Model(inputs=[input1, input2, input_base], outputs=[output])
 model.compile(optimizer='rmsprop', loss='binary_crossentropy')
-model.fit_generator(get_imgs(dir), steps_per_epoch=file_count/8, epochs=10)
+model.fit_generator(get_imgs(dir), steps_per_epoch=np.array([int(file_count/batch_size)]), epochs=10)
